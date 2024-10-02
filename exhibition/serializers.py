82b27612a4f382c246cb9f8152f1b17f6e1d10d1
maksim <mavel.production23@gmail.com>
2024-10-02 09:51:29 +0300

    fixed serializers.py for exhibition

--- exhibition/serializers.py
+++ exhibition/serializers.py
@@ -3,21 +3,23 @@ from .models import Breed, Kitten, Rating
 
 
 class BreedSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Breed
         fields = '__all__'
 
 
 class KittenSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source='user.username')
+    user = serializers.ReadOnlyField(source='user.name')
+    breed = serializers.ReadOnlyField(source='breed.name')
 
     class Meta:
         model = Kitten
-        fields = '__all__'
+        fields = ['pk', 'name', 'color', 'age', 'description', 'breed', 'user']
 
 
 class RatingSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source='user.username')
+    user = serializers.ReadOnlyField(source='user.name')
 
     class Meta:
         model = Rating
